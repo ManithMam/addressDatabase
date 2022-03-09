04c6209b04c942c0aa767bb85edc96a3fb515a7b
Manith Mam <manith7.mam@gmail.com>
2022-03-09 16:21:16 +0100

    update the person class by adding setter and getter methods

--- src/main/java/main/Person.java
+++ src/main/java/main/Person.java
@@ -13,6 +13,30 @@ public class Person {
         this.number = number;
     }
 
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getNumber(){
+        return this.number;
+    }
 
+    public void setFirstName(){
+        this.firstName = firstName;
+    }
+    public void setLastName(){
+        this.lastName = lastName;
+    }
+    public void setEmail(){
+        this.email = email;
+    }
+    public  void setNumber(){
+        this.number = number;
+    }
 
 }
