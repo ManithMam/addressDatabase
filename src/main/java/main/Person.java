1773b220f2595a8d3b08d3d73ba674ffcba118bf
Manith Mam <manith7.mam@gmail.com>
2022-03-09 16:05:36 +0100

    introduce all variables of an address and a class constructor

--- src/main/java/main/Person.java
+++ src/main/java/main/Person.java
@@ -1,4 +1,18 @@
 package main;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String number;
+
+    public Person(String firstName, String lastName, String email, String number){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.number = number;
+    }
+
+
+
 }
