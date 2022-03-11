55826abdcb53d9cc19815b2de99b937721fa59c7
Manith Mam <manith7.mam@gmail.com>
2022-03-11 16:27:41 +0100

    Add person ArrayList and creatPerson method

--- src/main/java/main/Person.java
+++ src/main/java/main/Person.java
@@ -1,10 +1,15 @@
 package main;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Person {
     private String firstName;
     private String lastName;
     private String email;
     private String number;
+
+    private ArrayList<Person> arr = new ArrayList<Person>();
 
     public Person(String firstName, String lastName, String email, String number){
         this.firstName = firstName;
@@ -43,5 +48,55 @@ public class Person {
     public String toString(){
         return this.firstName + " " + this.lastName + ", " + "email: " + this.email + ", phone: " + this.number;
     }
+
+    private void createPerson(){
+        Scanner input = new Scanner(System.in);
+
+        String firstName;
+        String lastName;
+        String mail;
+        String phone;
+
+        try {
+            System.out.print("First name: ");
+            firstName = input.nextLine();
+        }
+        catch(InputMismatchException e){
+            int numeric = input.nextInt();
+            firstName = String.valueOf(numeric);
+        }
+
+        try {
+            System.out.print("Last name: ");
+            lastName = input.nextLine();
+        }
+        catch(InputMismatchException e){
+            int numeric = input.nextInt();
+            lastName = String.valueOf(numeric);
+        }
+
+        try {
+            System.out.print("Optional input Email: ");
+            mail = input.nextLine();
+        }
+        catch(InputMismatchException e){
+            int numeric = input.nextInt();
+            mail = String.valueOf(numeric);
+        }
+
+        try {
+            System.out.print("Optional input phone: ");
+            phone = input.nextLine();
+        }
+        catch(InputMismatchException e){
+            int numeric = input.nextInt();
+            phone = String.valueOf(numeric);
+        }
+
+        Person person = new Person(firstName, lastName, phone, mail);
+
+        arr.add(person);
+    }
+
 
 }
